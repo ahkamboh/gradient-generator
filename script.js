522cbb8f63de3bbb86d8f88ffbbf66d9bea7b512
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2023-04-08 00:51:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Select by id
+let color1 = document.getElementById("color1");
+let color2 = document.getElementById("color2");
+let gradient_box = document.getElementById("gradient-box");
+let color_code = document.getElementById("color-code");
+let copyBtn = document.getElementById("copyBtn");
+function changeGradient() {
+  // get value of linear gradients
+  gradient_box.style.background = `radial-gradient( circle, ${color1.value}, ${color2.value})`;
+  color_code.value = `${gradient_box.style.background} ;`;
+}
+color1.addEventListener("input", changeGradient);
+color2.addEventListener("input", changeGradient);
+
+// Text Copy
+function myFunction() {
+  // Get the text field
+  let copyText = document.getElementById("color-code");
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+}
