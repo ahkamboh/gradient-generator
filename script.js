590bec96196b6832761cee9470342c4081240176
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2023-04-08 12:50:57 +0500

    Initial commit

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 let color1 = document.getElementById("color1");
 let color2 = document.getElementById("color2");
 let gradient_box = document.getElementById("gradient-box");
+let  body = document.querySelector("html");
 let color_code = document.getElementById("color-code");
 let copyBtn = document.getElementById("copyBtn");
 function changeGradient() {
   // get value of linear gradients
   gradient_box.style.background = `radial-gradient( circle, ${color1.value}, ${color2.value})`;
   color_code.value = `${gradient_box.style.background} ;`;
+  body.style.background = `radial-gradient( circle, ${color1.value}, ${color2.value})`;
+  color_code.value = `${body.style.background} ;`;
 }
 color1.addEventListener("input", changeGradient);
 color2.addEventListener("input", changeGradient);
@@ -22,4 +25,3 @@ function myFunction() {
   // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
 }
-
